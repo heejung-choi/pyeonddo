6af5d1acc187424a82f6dbf09e5f950103e4335a
heejung-choi <choi951008@naver.com>
2020-08-18 13:19:18 +0900

    편또 기획서

--- pyeonddo/promotions/urls.py
+++ pyeonddo/promotions/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'promotions'
 
 urlpatterns = [
-    
+    path('', views.promotion, name='promotion'),
 ]
--- pyeonddo/promotions/views.py
+++ pyeonddo/promotions/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def promotion(request):
+    return render(request,'promotions/promotion.html')
