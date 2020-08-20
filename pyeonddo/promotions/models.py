e945c5cd699a4c0c9059658c4cca4b3c0dc7c37d
GyuyoungEom <linda_jenny@naver.com>
2020-08-20 15:29:33 +0900

    update models

--- pyeonddo/promotions/models.py
+++ pyeonddo/promotions/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=500)
@@ -10,3 +10,9 @@ class Product(models.Model):
     category = models.CharField(max_length=500, null=True)
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='partner')
+    product = models.ManyToManyField(Product,related_name='event_product',null=True)
+    
