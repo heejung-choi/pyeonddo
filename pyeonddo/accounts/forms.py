32b9cfe40d10335a810b4404dedba441f62077d1
stella <smmin0402@gmail.com>
2020-08-24 22:41:08 +0900

    사용자회원가입

---
+++ pyeonddo/accounts/forms.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+from django import forms
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        label='아이디',
+    )
+    phone = forms.CharField(
+        label='휴대폰번호',
+    )
+    name_kr = forms.CharField(
+        label='이름',
+    )
+    category = forms.CharField(
+        label='즐겨찾는 상품',
+    )
+    prefer_store = forms.CharField(
+        label='즐겨찾는 매장',
+    )         
+
+    class Meta:
+        model = get_user_model()
+        # fields = '__all__'
+        fields = ('username','password1','password2','name_kr','phone','category','prefer_store')
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('phone','name_kr','category','prefer_store','store')        
+
--- pyeonddo/accounts/views.py
+++ pyeonddo/accounts/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,PasswordChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET,require_POST,require_safe,require_http_methods
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
 
 # Create your views here.
 def index(request):
@@ -6,3 +14,18 @@ def index(request):
 
 def mypage(request):
     return render(request,'accounts/mypage.html')
+
+@require_http_methods(['GET','POST'])
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('pyeonddo:index')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request,user)
+            return redirect('accounts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {'form':form}
+    return render(request, 'accounts/form.html', context)
